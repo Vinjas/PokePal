3d8c5907716117fd39e0a3eb5d13160f213e2d12
Daniel Vinyas <danielvinyastorres@gmail.com>
2023-10-28 16:13:28 +0200

    fix: dropdown click handle on whole screen

--- src/utils/getFlag.tsx
+++ src/utils/getFlag.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import EN from '@assets/svg/flags/en.svg';
 import ES from '@assets/svg/flags/es.svg';
+import DE from '@assets/svg/flags/de.svg';
+import FR from '@assets/svg/flags/fr.svg';
+import IT from '@assets/svg/flags/it.svg';
+import JA from '@assets/svg/flags/ja.svg';
+import KO from '@assets/svg/flags/ko.svg';
 import { View } from 'react-native';
 
 export const getFlag = (language: string | { value: string }, size: string) => {
@@ -17,6 +22,41 @@ export const getFlag = (language: string | { value: string }, size: string) => {
     case 'es':
       return (
         <ES
+          width={size}
+          height={size}
+        />
+      );
+    case 'de':
+      return (
+        <DE
+          width={size}
+          height={size}
+        />
+      );
+    case 'fr':
+      return (
+        <FR
+          width={size}
+          height={size}
+        />
+      );
+    case 'it':
+      return (
+        <IT
+          width={size}
+          height={size}
+        />
+      );
+    case 'ja':
+      return (
+        <JA
+          width={size}
+          height={size}
+        />
+      );
+    case 'ko':
+      return (
+        <KO
           width={size}
           height={size}
         />
